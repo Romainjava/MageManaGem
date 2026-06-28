f820222256fc7eca75c0c15495c5d645ff77bf74
Romain <phenixstrato@gmail.com>
2026-06-28 09:43:45 +0200

    Improve mana gem detection for BC Classic

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 1.1.0
+
+- Improved `/delgem` bag scanning by checking the bag item ID before falling back to link matching.
+- Keeps Mana Emerald detection language-independent on BC Classic clients if item link parsing changes.
--- MageManaGem.lua
+++ MageManaGem.lua
@@ -2,6 +2,9 @@ local _, addon = ...
 
 SLASH_MGEM1 = "/delgem"
 
+local MANA_EMERALD_ID = 22044
+local MANA_EMERALD_LINK = "item:" .. MANA_EMERALD_ID
+
 SlashCmdList["MGEM"] = function()
     if InCombatLockdown() then
         print(addon.L.COMBAT_ERROR)
@@ -10,8 +13,10 @@ SlashCmdList["MGEM"] = function()
 
     for b = 0, 4 do
         for s = 1, C_Container.GetContainerNumSlots(b) do
-            local link = C_Container.GetContainerItemLink(b, s)
-            if link and link:find("item:22044") then
+            local itemInfo = C_Container.GetContainerItemInfo(b, s)
+            local link = itemInfo and itemInfo.hyperlink or C_Container.GetContainerItemLink(b, s)
+
+            if (itemInfo and itemInfo.itemID == MANA_EMERALD_ID) or (link and link:find(MANA_EMERALD_LINK, 1, true)) then
                 C_Container.PickupContainerItem(b, s)
                 DeleteCursorItem()
                 return
